6371c47ea5746c55a234f2ebf626213440702aa5
Areola Kelvin Mayokun <kareola960@gmail.com>
2024-09-28 23:51:05 +0100

    Final implementation

--- src/pages/Cards.tsx
+++ src/pages/Cards.tsx
@@ -1,6 +1,13 @@
-
 export default function Cards() {
   return (
-    <div>Cards</div>
-  )
+    <div className="w-full flex-col gap-6 text-primary my-24 flex items-center justify-center">
+      <div className="flex flex-wrap flex-col gap-6 justify-center items-center">
+        <h2 className="text-3xl lg:text-4xl font-semibold">Virtual Cards Coming Soon!</h2>
+        <p className="text-xl">Get ready for safer, smarter payments. Stay tuned!</p>
+      </div>
+      <div className="w-64 lg:w-96">
+        <img src="./virtual-card.svg" alt="Virtual Cards"/>
+      </div>
+    </div>
+  );
 }
